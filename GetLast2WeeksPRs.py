70eaf63a1b4a518b2b38eaf48cdbc3aaad479dc5
kopperqu <email@email.com>
2023-01-18 20:50:01 -0600

    Project grabs prs from today minus x amount of days, need to format to be able to be put onto website

--- GetLast2WeeksPRs.py
+++ GetLast2WeeksPRs.py
@@ -1,7 +1,18 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from datetime import datetime,timedelta
+from itertools import groupby
 
+value=''
+while not value.isdigit():
+    value = input("enter number of days since meet you want prs from add 1 for safey :) ")
+    if value.isdigit():
+        numerical_value = int(value)
+    else:
+        print("Invalid input. Please enter a valid number.")
+
+
+daysToRemove = numerical_value
 mensTrackURL = 'https://www.tfrrs.org/teams/tf/WI_college_m_Wis_Stevens_Point.html'
 html = urlopen(mensTrackURL)
 soup=BeautifulSoup(html.read(), "html.parser")
@@ -24,8 +35,8 @@ men,menTffrsLink = getAthletesNamesAndTffrsLinks(mensTrackURL)
 women,womenTffrsLink = getAthletesNamesAndTffrsLinks(womensTrackURL)
 names = men+women
 links = menTffrsLink + womenTffrsLink
-twoweeksago = datetime.today().date() - timedelta(days=14)
-
+twoweeksago = datetime.today().date() - timedelta(daysToRemove)
+initalDictionary = []
 for index,eachName in enumerate(names):
     html = urlopen(links[index])
     soup=BeautifulSoup(html.read(), "html.parser")
@@ -33,18 +44,34 @@ for index,eachName in enumerate(names):
     prMeets=[]
     for eachMeet in allMeets:
         if (eachMeet.find('tr',class_='highlight')!=None):
-            print(eachName + ' pr\'d in the ' + eachMeet.find('a').text + ' on ' +eachMeet.find('span').text.strip())
-        inputDate = eachMeet.find_parent().find('span').text.strip()
+            prMeets.append(eachMeet)
+    for eachPRMeet in prMeets:
+        inputDate = eachPRMeet.find('span').text.strip()
+        #Special case for dates like April 13-14, 2021
+        if '-' in inputDate:
+            split = inputDate.split('-')
+            split2 = split[1].split(',')
+            inputDate = split[0] +','+split2[1]
         format="%b %d, %Y"
-        meetDay = datetime.strptime(inputDate,format).date()
+        meetDate = datetime.strptime(inputDate,format).date()
+        if(meetDate>twoweeksago):
+            #!!!RECENTPR!!!
+            #Need to grab the athlete name, meet name, event, pr, meet date
+            meetName = eachPRMeet.find('thead').find('a').text.strip()
+            allPRsAtPRMeet = eachPRMeet.findAll('tr', class_='highlight')
+            for eachPRatPRMeet in allPRsAtPRMeet:
+                eventName = eachPRatPRMeet.find('td').text.strip()
+                pr = eachPRatPRMeet.find('a').text.strip()
+                initalDictionary.append((eachName,meetName,eventName,pr,meetDate))
+                print(eachName)
 
-testurl = 'https://www.tfrrs.org/athletes/8280699/Wis_Stevens_Point/Joseph_Adams.html'
+sortedDictByDate = {k: list(g) for k, g in groupby(sorted(initalDictionary, key=lambda x: x[4], reverse=True), key=lambda x: x[4])}
+for eachDate,meetsAtDate in sortedDictByDate.items():
+    sortedByMeet = {k: list(g) for k, g in groupby(sorted(meetsAtDate, key=lambda x: x[1]), key=lambda x: x[1])}
+    for eachMeet,prs in sortedByMeet.items():
+        print('\n\n\n')
+        print (eachMeet + ' date ' + prs[0][4].strftime(format))
+        for eachPR in prs:
+            print (eachPR[0] + " pr'd in the " + eachPR[2] + " with a " + eachPR[3])
 
-html = urlopen(testurl)
-soup=BeautifulSoup(html.read(), "html.parser")
-prMeets[0].find_parent().find('span').text.strip()
-allMeets = soup.find('div',id='meet-results').findAll('div')
-
-inputDate = a[0].find('span').text.strip()
-format="%b %d, %Y"
-datetime.strptime(inputDate,format).date()
+input('enter to close')
